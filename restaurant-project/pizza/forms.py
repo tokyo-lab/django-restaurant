07cc1e6633ccc22a853948a8439df8912de82a75
cooper <tokyolabs@protonmail.com>
2023-03-18 18:43:25 -0400

    wip form

--- restaurant-project/pizza/forms.py
+++ restaurant-project/pizza/forms.py
@@ -4,9 +4,9 @@ from django import forms
 class PizzaForm(forms.Form):
     size = forms.ChoiceField(
         label="Size",
-        choices=[("Small", "Small"), ("Medium", "Medium"), ("Large", "Large")],
+        choices=[("small", "Small"), ("medium", "Medium"), ("large", "Large")],
     )
     crust = forms.ChoiceField(
         label="Crust",
-        choices=[("Regular", "Regular"), ("Thin", "Thin")],
+        choices=[("regular", "Regular"), ("thin", "Thin")],
     )
--- restaurant-project/pizza/views.py
+++ restaurant-project/pizza/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from .forms import PizzaForm
 
-# Create your views here.
-
 
 def home(request):
     return render(request, "pizza/home.html")
 
 
 def order(request):
-    form = PizzaForm()
-    return render(request, "pizza/order.html", {"pizzaform": form})
+    if request.method == "POST":
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = "Thanks for ordering! Your %s %s crust pizza is on its way!" % (
+                filled_form.cleaned_data["size"],
+                filled_form.cleaned_data["crust"],
+            )
+        else:
+            note = "Order was not created, please try again"
+        new_form = PizzaForm()
+        return render(
+            request, "pizza/order.html", {"pizzaform": new_form, "note": note}
+        )
+    else:
+        form = PizzaForm()
+        return render(request, "pizza/order.html", {"pizzaform": form})
