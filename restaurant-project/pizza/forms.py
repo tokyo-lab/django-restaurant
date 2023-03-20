a5bc5fc0da4f8b3b6dd5c6728bc2538f93c102b3
cooper <tokyolabs@protonmail.com>
2023-03-20 08:49:05 -0400

    wip

--- restaurant-project/pizza/forms.py
+++ restaurant-project/pizza/forms.py
@@ -1,25 +1,40 @@
 from django import forms
-from .models import Pizza, Customer
+
+# from .models import Pizza, Customer
 
 
-class PizzaForm(forms.ModelForm):
-    class Meta:
-        model = Pizza
-        fields = ["size", "crust"]
+# class PizzaForm(forms.ModelForm):
+#     class Meta:
+#         model = Pizza
+#         fields = ["size", "crust"]
+#         labels = {""}
 
 
-class CustomerForm(forms.ModelForm):
-    class Meta:
-        model = Customer
-        fields = ["name", "email"]
+# class CustomerForm(forms.ModelForm):
+#     class Meta:
+#         model = Customer
+#         fields = ["name", "email"]
 
 
-# class PizzaForm(forms.Form):
-#     size = forms.ChoiceField(
-#         label="Size",
-#         choices=[("small", "Small"), ("medium", "Medium"), ("large", "Large")],
-#     )
-#     crust = forms.ChoiceField(
-#         label="Crust",
-#         choices=[("regular", "Regular"), ("thin", "Thin")],
-#     )
+class PizzaForm(forms.Form):
+    size = forms.ChoiceField(
+        label="Size",
+        choices=[("small", "Small"), ("medium", "Medium"), ("large", "Large")],
+        widget=forms.RadioSelect,
+    )
+    crust = forms.ChoiceField(
+        label="Crust",
+        choices=[("regular", "Regular"), ("thin", "Thin")],
+        widget=forms.RadioSelect,
+    )
+
+
+class CustomerForm(forms.Form):
+    name = forms.CharField(
+        label="name",
+        max_length=100,
+    )
+    email = forms.EmailField(
+        label="email",
+        max_length=100,
+    )
