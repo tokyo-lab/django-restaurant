18c78ff3973d37642e5bb9a0df2b11aa0d0d3d59
cooper <tokyolabs@protonmail.com>
2023-03-26 17:54:54 -0400

    wip

--- restaurant-project/pizza/views.py
+++ restaurant-project/pizza/views.py
@@ -15,8 +15,6 @@ def order(request):
             filled_form.save()
             note = "Thanks for ordering! Your %s %s and %s pizza is on its way!" % (
                 filled_form.cleaned_data["size"],
-                filled_form.cleaned_data["topping1"],
-                filled_form.cleaned_data["topping2"],
             )
             new_form = PizzaForm()
             return render(
@@ -48,7 +46,7 @@ def pizzas(request):
         filled_formset = PizzaFormSet(request.POST)
         if filled_formset.is_valid():
             for form in filled_formset:
-                print(form.cleaned_data["topping1"])
+                print(form.cleaned_data["crust"])
             note = "Pizzas have been ordered!"
         else:
             note = "Order was not created, please try again"
